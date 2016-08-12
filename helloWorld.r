f69ee9532bd4703ebdfdb3a755e4f7196cd8acbf
Scott Sutton <scott.sutton@samsung.com>
2016-08-12 15:45:57 -0700

    First commit to Project-R

---
+++ helloWorld.r
@@ -0,0 +1,38 @@
+assign("x", 10.4)
+assign("y", c(10.4, 5.6, 6.4, 2.7))
+assign("z", 10.4)
+z = 1.4
+a = c(1, 2, 3, 4, 4)
+vector(length = 5)
+init1 = vector(length = 5)
+init2 = c(FALSE, FALSE, TRUE, TRUE, TRUE)
+init1 + init2
+init1 * init2
+mean(init1)
+mean(init2)
+x1 = init1
+x2 = init2
+init1[3] = 7
+init2[3] = 7
+x3 = c(0,1,1,0)
+x3 = c(TRUE, FALSE, TRUE, FALSE)
+seq(8)
+seq(8,)
+seq(from=7,length=2,by=.1)
+seq(from=7,length=20,by=.1)
+seq(0,7,by=.1)
+xtimes5 = rep(x, times=5)
+xeach5 = rep(x, each=5)
+temp = x5
+temp * 2
+c(1:3,NA)
+c(1:3,NA)[1]
+c(1:3,NA)[4]
+is.na(c(1:3,NA))
+is.na(c(1:3,NA,NA))
+is.na(c(1:3,NA,NA,1:3))
+is.na(c(1:3,NA,NA,2:3))
+c(1:3,NA,NA,1:3)
+c(1:3,NA,NA,3:0)
+c(1:3,NA,NA,3.0:0.0)
+
